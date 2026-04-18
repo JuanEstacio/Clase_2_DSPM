97e26d086bc619f9848d85bd30ee5258e2593726
JuanEstacio <150970428+JuanEstacio@users.noreply.github.com>
2026-04-18 17:51:06 -0500

    Final

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { IonReactRouter } from '@ionic/react-router';
 
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-import TasksPage from './pages/TasksPage';
-import TaskFormPage from './pages/TaskFormPage';
 import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
