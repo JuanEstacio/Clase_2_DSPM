7805f87a3aa9935f97421c06c95f60d2b1a5fd22
JuanEstacio <150970428+JuanEstacio@users.noreply.github.com>
2026-05-02 15:57:37 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Challenge_10',
+  webDir: 'dist'
+};
+
+export default config;
